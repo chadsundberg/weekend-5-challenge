827016dddc00fe327d645789d8639ce16f73e393
Chad Sundberg <chadsundberg@gmail.com>
2017-03-12 15:31:06 -0500

    form clears on submit

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -2,13 +2,15 @@ myApp.controller('HomeController', ['DataFactory', function(DataFactory) {
   var self = this;
   self.employeeList = DataFactory.allEmployees;
   self.newEmployee = {};
-  
+
 
 
 
   self.addEmployee = function() {
     DataFactory.addEmployee(self.newEmployee);
     console.log(self.newEmployee);
+    self.newEmployee = {};
   }
+
 
 }]); // end controller code block
