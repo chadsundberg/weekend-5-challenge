05b9d5dea68373df35303a5f50171ad124d82cec
Chad Sundberg <chadsundberg@gmail.com>
2017-03-13 07:14:04 -0500

    add database-sql INSERT INTO data

--- database.sql
+++ database.sql
@@ -7,3 +7,5 @@ job_title VARCHAR(80) NOT NULL,
 annual_salary int,
 status BOOLEAN DEFAULT true
 );
+
+INSERT INTO employees (first_name, last_name, id_number, job_title, annual_salary, status) VALUES ('Chad', 'Sundberg', '1234', 'something', '30000', 'true');
