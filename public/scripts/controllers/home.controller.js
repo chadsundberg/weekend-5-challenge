043394be24e42d3e79456a9c5225a369fb6063fa
Chad Sundberg <chadsundberg@gmail.com>
2017-03-13 00:06:38 -0500

    added monthly salary expense section

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -2,6 +2,7 @@ myApp.controller('HomeController', ['DataFactory', function(DataFactory) {
   var self = this;
   self.employeeList = DataFactory.allEmployees;
   self.newEmployee = {};
+  self.monthlySalaryExpenses = 0;
 
   self.addEmployee = function() {
     DataFactory.addEmployee(self.newEmployee);
@@ -17,6 +18,15 @@ myApp.controller('HomeController', ['DataFactory', function(DataFactory) {
     console.log('deactivate employee clicked on', employeeID);
     DataFactory.deactivateEmployee(employeeID);
   }
+
+  self.calculateMonthlySalaryExp = function(){
+  var totalSalary = 0;
+  for (var i = 0; i < self.employeeList.list.length; i++) {
+    totalSalary += self.employeeList.list[i].annual_salary;
+  }
+  self.monthlySalaryExpenses = totalSalary/12;
+  return self.monthlySalaryExpenses;
+}
 
 
 }]); // end controller code block
